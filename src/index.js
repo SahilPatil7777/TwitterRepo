9674aef04562e982d1cc1b84e610808f46fbab85
SahilPatil7777 <sahilpatil32267@gmail.com>
2025-07-09 16:07:48 +0530

    Added controller and routes for create tweet api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { connect } from "./config/database.js";
+import apiRoutes from "./routes/index.js";
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api", apiRoutes);
 
 import service from "./services/tweet-service.js";
 
@@ -8,6 +15,4 @@ app.listen(3000, async () => {
   console.log("server started");
   await connect();
   console.log("Mongo db connected");
-  let ser = new service();
-  await ser.create({ content: "capital #BRO" });
 });
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -11,8 +11,7 @@ class TweetService {
       const content = data.content;
       const tags = content
         .match(/#[a-zA-Z0-9_]+/g)
-        .map((tag) => tag.substring(1))
-        .map((tag) => tag.toLowerCase()); // this regex extract hashtag from the content
+        .map((tag) => tag.substring(1).toLowerCase()); // this regex extract hashtag from the content
 
       const tweet = await this.tweetRepository.create(data);
       let alreadyPresentTags = await this.hashtagRepository.findByName(tags);
